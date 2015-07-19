2fdf97e48f0d0046848e49d2306182966dfb0ee4
ANTONIO JIMÉNEZ MARTÍNEZ <antonio.jimenez2@gmail.com>
2015-07-19 19:54:01 +0200

    Transformer form url to NSData

---
+++ PDKTModelBuilder/DataTransformers/PDKTDataFromURLImageTransformer.h
@@ -0,0 +1,13 @@
+//
+//  PDKTDataFromURLImageTransformer.h
+//  PDKTModelBuilder
+//
+//  Created by ANTONIO JIMÉNEZ MARTÍNEZ on 19/7/15.
+//  Copyright (c) 2015 Produkt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PDKTDataFromURLImageTransformer : NSObject
+
+@end
